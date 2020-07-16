2f27c89412cfd7d58905dbb7764012f6b730afbb
Aweiisacat <face992302@gmail.com>
2020-07-16 22:04:53 +0800

    hello React

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const hello = <input className="input" type="number" />;
+
+const root = document.getElementById('root');
+
+ReactDOM.render(hello, root);
